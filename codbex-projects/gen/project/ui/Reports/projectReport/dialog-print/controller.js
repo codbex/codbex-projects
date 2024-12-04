83b30d1b4c914faf8b885857e9a4a04d7462d220
TIVMOF <tomyivanov06@gmail.com>
2024-12-04 14:46:11 +0000

    feat: Adding reports

---
+++ codbex-projects/gen/project/ui/Reports/projectReport/dialog-print/controller.js
@@ -0,0 +1,51 @@
+angular.module('page', ["ideUI", "ideView", "entityApi"])
+    .config(["messageHubProvider", function (messageHubProvider) {
+        messageHubProvider.eventIdPrefix = 'codbex-projects.Reports.projectReport';
+    }])
+    .config(["entityApiProvider", function (entityApiProvider) {
+        entityApiProvider.baseUrl = "/services/ts/codbex-projects/gen/project/api/projectReport/projectReportService.ts";
+    }])
+    .controller('PageController', ['$scope', 'messageHub', 'entityApi', 'ViewParameters', function ($scope, messageHub, entityApi, ViewParameters) {
+
+		let params = ViewParameters.get();
+		if (Object.keys(params).length) {         
+            const filterEntity = params.filterEntity ?? {};
+
+			const filter = {
+			};
+			if (filterEntity.PROJECT_NAME) {
+				filter.PROJECT_NAME = filterEntity.PROJECT_NAME;
+			}
+
+            $scope.filter = filter;
+		}
+
+        $scope.loadPage = function (filter) {
+            if (!filter && $scope.filter) {
+                filter = $scope.filter;
+            }
+            let request;
+            if (filter) {
+                request = entityApi.search(filter);
+            } else {
+                request = entityApi.list();
+            }
+            request.then(function (response) {
+                if (response.status != 200) {
+                    messageHub.showAlertError("projectReport", `Unable to list/filter projectReport: '${response.message}'`);
+                    return;
+                }
+                $scope.data = response.data;
+                setTimeout(() => {
+                    window.print();
+
+                }, 250);
+            });
+        };
+        $scope.loadPage($scope.filter);
+
+        window.onafterprint = () => {
+            messageHub.closeDialogWindow("codbex-projects-Reports-projectReport-print");
+        }
+
+    }]);
