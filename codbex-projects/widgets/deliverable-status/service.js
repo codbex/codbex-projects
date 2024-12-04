4e00d24c16343e572798216e052b2cb56fe1eb27
TIVMOF <tomyivanov06@gmail.com>
2024-12-04 09:23:25 +0000

    fix: Id and link in services

--- codbex-projects/widgets/deliverable-status/service.js
+++ codbex-projects/widgets/deliverable-status/service.js
@@ -1,7 +1,7 @@
 const widgetData = {
-    id: 'deliverable-status',
+    id: 'deliverable-status-widget',
     label: 'Deliverable Status',
-    link: '/services/web/codbex-projects/widgets/subviews/deliverable-status.html',
+    link: '/services/web/codbex-projects/widgets/deliverable-status/index.html',
     redirectViewId: "deliverable-navigation",
     size: "large"
 };
